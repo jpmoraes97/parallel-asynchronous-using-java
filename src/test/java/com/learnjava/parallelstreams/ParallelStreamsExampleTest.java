74bfcb55e8b3058798cb3859922709cb502f1c44
joao.santos2 <joao.santos@ti9.com.br>
2021-02-06 17:37:17 -0300

    Sequential() and Parallel() functions in streams API

--- src/test/java/com/learnjava/parallelstreams/ParallelStreamsExampleTest.java
+++ src/test/java/com/learnjava/parallelstreams/ParallelStreamsExampleTest.java
@@ -2,6 +2,8 @@ package com.learnjava.parallelstreams;
 
 import com.learnjava.util.DataSet;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -23,6 +25,22 @@ class ParallelStreamsExampleTest {
         startTimer();
         List<String> resultList = parallelStreamsExample.stringTransform(inputList);
         timeTaken();
+
+        //then
+        assertEquals(4, resultList.size());
+        resultList.forEach(name -> assertTrue(name.contains("-")));
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {false, true})
+    void stringTransform1(boolean isParallel) {
+        // given
+        List<String> inputList = DataSet.namesList();
+
+        // when
+        //startTimer();
+        List<String> resultList = parallelStreamsExample.stringTransform1(inputList, isParallel);
+        //timeTaken();
 
         //then
         assertEquals(4, resultList.size());
